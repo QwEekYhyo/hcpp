39a684e43516697d8dae3e534d19980b28e0662b
Logan Lucas <logan.lucas83@gmail.com>
2025-02-05 08:40:59 +0100

    feat: signal handler but singleton is ugly
    maybe add the signal handler in main so no need for singleton?

--- include/Server.hpp
+++ include/Server.hpp
@@ -11,7 +11,14 @@ class Server {
         void listen(in_port_t port);
 
     private:
+        static Server* instance;
         int m_fd{-1};
+        struct sockaddr_in m_address;
+        int m_working{false};
+
+        void setup_interrupt_handler();
+        void interrupt_handler(int sig);
+        static void static_interrupt_handler(int sig);
 };
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,18 +1,28 @@
 #include "../include/Server.hpp"
 
-#include <netinet/in.h>
+#include <iostream>
 #include <stdexcept>
 
 #include <sys/socket.h>
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+
+Server* Server::instance = nullptr;
 
 Server::Server() {
     m_fd = socket(AF_INET, SOCK_STREAM, 0);
+    const int enable = 1;
+    if (setsockopt(m_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+        
 
     if (m_fd < 0)
         throw std::runtime_error(strerror(errno));
+    Server::instance = this;
+    setup_interrupt_handler();
 }
 
 Server::~Server() {
@@ -21,12 +31,11 @@ Server::~Server() {
 }
 
 void Server::listen(in_port_t port) {
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(port);
-    address.sin_addr.s_addr = INADDR_ANY;
+    m_address.sin_family = AF_INET;
+    m_address.sin_port = htons(port);
+    m_address.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(m_fd, (const struct sockaddr*) &address, sizeof(address)) < 0)
+    if (bind(m_fd, (const struct sockaddr*) &m_address, sizeof(m_address)) < 0)
         throw std::runtime_error(strerror(errno));
 
     if (::listen(m_fd, 4096) < 0)
@@ -35,7 +44,7 @@ void Server::listen(in_port_t port) {
 
     struct sockaddr_in client_address = {0};
     socklen_t client_address_length;
-    for (;;) {
+    for (m_working = true; m_working;) {
         int client_fd =
             accept(m_fd, (struct sockaddr*) &client_address, &client_address_length);
         if (client_fd < 0) {
@@ -48,8 +57,10 @@ void Server::listen(in_port_t port) {
             perror("read");
             exit(1);
         }
+        std::cout << buf;
 
-        char reply[] = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n";
+        // char reply[] = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n";
+        char reply[] = "HTTP/1.1 200 OK\r\nContent-Length: 15\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nHello from HCPP\r\n";
 
         if (write(client_fd, &reply, sizeof(reply)) < 0) {
             perror("write");
@@ -58,4 +69,26 @@ void Server::listen(in_port_t port) {
 
         close(client_fd);
     }
+    std::cout << "Stopped listening\r\n";
+}
+
+void Server::setup_interrupt_handler() {
+    sigset_t msk_int;
+    sigemptyset(&msk_int);
+    sigaddset(&msk_int, SIGINT);
+
+    struct sigaction sigact;
+    sigact.sa_handler = Server::static_interrupt_handler;
+    sigact.sa_mask = msk_int;
+
+    sigaction(SIGINT, &sigact, NULL);
+}
+
+void Server::interrupt_handler(int sig) {
+    if (m_fd >= 0)
+        close(m_fd);
+}
+
+void Server::static_interrupt_handler(int sig) {
+    Server::instance->interrupt_handler(sig);
 }
